bf9987da1c023664f3b9b1a5ea62a4dd41650e28
k4ppa <cappa.mattia@gmail.com>
2015-06-22 17:31:47 +0200

    Week 2 Exercise B

--- TheActivityClass/Lab2_ActivityLab/src/course/labs/activitylab/ActivityTwo.java
+++ TheActivityClass/Lab2_ActivityLab/src/course/labs/activitylab/ActivityTwo.java
@@ -76,12 +76,10 @@ public class ActivityTwo extends Activity {
 			// TODO:
 			// Restore value of counters from saved state
 			// Only need 4 lines of code, one for every count variable
-
-
-			
-			
-			
-			
+            mCreate = savedInstanceState.getInt(CREATE_KEY);
+            mRestart = savedInstanceState.getInt(RESTART_KEY);
+            mResume = savedInstanceState.getInt(RESUME_KEY);
+            mStart = savedInstanceState.getInt(START_KEY);
 		}
 
 		// Emit LogCat message
@@ -168,13 +166,10 @@ public class ActivityTwo extends Activity {
 		// TODO:
 		// Save counter state information with a collection of key-value pairs
 		// 4 lines of code, one for every count variable
-
-
-
-		
-		
-		
-		
+        savedInstanceState.putInt(RESTART_KEY, mRestart);
+        savedInstanceState.putInt(START_KEY, mStart);
+        savedInstanceState.putInt(RESUME_KEY, mResume);
+        savedInstanceState.putInt(CREATE_KEY, mCreate);
 	}
 
 	// Updates the displayed counters
