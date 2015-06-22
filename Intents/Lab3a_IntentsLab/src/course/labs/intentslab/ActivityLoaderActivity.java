0bcc1d6cc1fe2452efb5b8e30c81f6338940ce39
k4ppa <cappa.mattia@gmail.com>
2015-06-22 17:59:05 +0200

    Week 3 Exercise on explicit intent

--- Intents/Lab3a_IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
+++ Intents/Lab3a_IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
@@ -2,7 +2,6 @@ package course.labs.intentslab;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -66,12 +65,10 @@ public class ActivityLoaderActivity extends Activity {
 		Log.i(TAG,"Entered startExplicitActivation()");
 		
 		// TODO - Create a new intent to launch the ExplicitlyLoadedActivity class
-		Intent explicitIntent = null;
+		Intent explicitIntent = new Intent(ActivityLoaderActivity.this, ExplicitlyLoadedActivity.class);
 		
 		// TODO - Start an Activity using that intent and the request code defined above
-		
-        
-        
+		startActivityForResult(explicitIntent, GET_TEXT_REQUEST_CODE);
 	}
     
 	// Start a Browser Activity to view a web page or its URL
@@ -107,11 +104,9 @@ public class ActivityLoaderActivity extends Activity {
 		// TODO - Process the result only if this method received both a
 		// RESULT_OK result code and a recognized request code
 		// If so, update the Textview showing the user-entered text.
-
-	
-    
-    
-    
-    
+		if (resultCode == RESULT_OK && requestCode == GET_TEXT_REQUEST_CODE) {
+			String userText = data.getStringExtra("EditText");
+			mUserTextView.setText(userText);
+		}
     }
 }
